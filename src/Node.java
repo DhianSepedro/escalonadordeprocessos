286a0b67fff1626eb08c0a0723449aa2e297ab7d
Dhian Sepedro <dhianpablo@gmail.com>
2025-09-16 15:12:01 -0300

    Fix classe no, para transformar em dupla

--- src/Node.java
+++ src/Node.java
@@ -1,9 +1,11 @@
 public class Node {
     Processos processo;
     Node proximo;
+    Node anterior;
     public Node(Processos processo) {
         this.processo = processo;
         this.proximo = null;
+        this.anterior = null;
     }
 }
 
